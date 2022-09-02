a8204b87e585d4acfd84d0194199790d6a07b7d7
Volodymyr Kravets <volodymyr.kravets@outlook.com>
2022-09-02 18:39:37 +0300

    Fixed sonar warnings

--- rskj-core/src/main/java/co/rsk/rpc/modules/rsk/RskModuleImpl.java
+++ rskj-core/src/main/java/co/rsk/rpc/modules/rsk/RskModuleImpl.java
@@ -46,10 +46,11 @@ public class RskModuleImpl implements RskModule {
 
     @Override
     public void flush() {
-        if (flusher==null)
+        if (flusher == null) {
             return;
-        flusher.forceFlush();
+        }
 
+        flusher.forceFlush();
     }
 
     @Override
